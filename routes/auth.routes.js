0109a194f95ae873ad0e93aaf23e3e76a2afeb77
bazzscript <bmexit@gmail.com>
2022-04-07 16:04:46 +0100

    Completed Truck driver Sign Up Api

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,8 @@ const authController = require("../controllers/auth.controller");
 
 
 // auth routes
+
+// signup
 router.post('/signup', authController.signup);
 router.get('/verifyUser/', authController.verifyUser);
 router.put('/resendVerificationEmail', authController.resendVerificationEmail);
