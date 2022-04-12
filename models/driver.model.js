99ca0d254d5aa15db1b0ba114a57ce87b6b41240
bazzscript <bmexit@gmail.com>
2022-04-12 14:37:23 +0100

    initial commit of so many changes

--- models/driver.model.js
+++ models/driver.model.js
@@ -6,22 +6,10 @@ const truckDriverSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
-
-    // wallet: {
-        
-    //   },
-
-
-    // documentation: {
-    //     TIN: { type: String, default: "" },
-    //     homeAddress: { type: String, default: "" },
-    //     officeAddress: { type: String, default: "" },
-    //     BVN: { type: String, default: "" },
-    //     NIN: { type: String, default: "" },
-    //     IDCard: { type: String, default: "" },
-    //     CAC: { type: String, default: "" },
-    //     utilityBill: { type: String, default: "" },
-    //   },
+    truck: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 
