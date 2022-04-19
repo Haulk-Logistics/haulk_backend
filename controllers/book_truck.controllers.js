d229b69d59ca98425d65e3923a508c4366277890
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-19 13:36:16 +0100

    added

--- controllers/book_truck.controllers.js
+++ controllers/book_truck.controllers.js
@@ -1,7 +1,9 @@
 const { validationResult } = require("express-validator");
 const formidable = require("formidable");
+const truckModel = require('../models/truck.model');
 const { calculate_amount } = require("../utils/calculate_amount.util");
 const book_truck_controller = {};
+const axios = require("axios");
 const { upload_image } = require("../services/cloudinary.services");
 const getDistanceFromLatLonInKm = require("../utils/calculate_distance");
 book_truck_controller.get_quotation = async (req, res) => {
@@ -11,7 +13,12 @@ book_truck_controller.get_quotation = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
   try {
-    const distance = await getDistanceFromLatLonInKm(data.latSrc, data.longSrc, data.latDes, data.longDes);
+    const distance = await getDistanceFromLatLonInKm(
+      data.latSrc,
+      data.longSrc,
+      data.latDes,
+      data.longDes
+    );
     console.log(distance);
     const amount = await calculate_amount(distance);
     res.status(200).json({
@@ -31,16 +38,44 @@ book_truck_controller.get_quotation = async (req, res) => {
 book_truck_controller.make_order = async (req, res) => {
   const form = new formidable.IncomingForm();
   form.parse(req, async function (err, fields, files) {
+    // upload to cloudinary
+    let url;
     const file_path = files.image.filepath;
     if (file_path) {
-      // uploads to cloudinary
       const result = await upload_image(file_path, "Driver");
-      console.log(result);
+      url = result && result.url;
     } else {
       console.log("No path inputed");
     }
+    const {
+      nature_of_goods,
+      truck_type,
+      drop_off_location,
+      pick_off_location,
+      pick_up_date,
+      container_size,
+      container_number,
+      shipping_line,
+    } = fields;
+    const data = {
+      ordered_by: req.user._id,
+      nature_of_goods,
+      truck_type,
+      drop_off_location,
+      pick_off_location,
+      pick_up_date,
+      container_number,
+      container_size,
+      shipping_line,
+      url,
+    };
+    console.log(data);
   });
-  // console.log(req.body)
+};
+
+book_truck_controller.getDriver = async (req, res) => {
+  const trucks =  await truckModel.find();
+  console.log(trucks)
 };
 
 module.exports = book_truck_controller;
--- routes/api/v1/book_truck.routes.js
+++ routes/api/v1/book_truck.routes.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 const {
   get_quotation,
   make_order,
+  getDriver
 } = require("../../../controllers/book_truck.controllers");
 const { isAuthorized } = require("../../../middlewares/auth.middlewares");
 // Router
@@ -19,19 +20,8 @@ router.post(
 );
 
 //  booking a truck and make payment for the truck
-router.post(
-  "/book_a_truck",
-  isAuthorized,
-  body("nature_of_good").not().isEmpty().trim().escape(),
-  // body("truck_type").not().isEmpty().trim().escape(),
-  // body("drop_off_location").not().isEmpty().trim().escape(),
-  // body("pick_off_location").not().isEmpty().trim().escape(),
-  // body("pick_up_date").not().isEmpty().trim().escape(),
-  // body("container_size").not().isEmpty().trim().escape(),
-  // body("container_number").not().isEmpty().trim().escape(),
-  // body("shipping_line").not().isEmpty().trim().escape(),
-  body("image").not().isEmpty().trim().escape(),
-  make_order
-);
+router.post("/book_a_truck", isAuthorized, make_order);
+
+router.get('/getter', isAuthorized, getDriver)
 
 module.exports = router;
