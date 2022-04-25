c72994a3a7a274f121778e8b4d9cc3f726041e0e
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-25 08:53:10 +0100

    finsied cargo_owner details fetch

--- controllers/cargoowner.controller.js
+++ controllers/cargoowner.controller.js
@@ -1,5 +1,6 @@
 const cargoOwnwer = {};
 const Order = require("../models/order.model");
+const Cargoowner = require("../models/cargo_owner.model");
 
 cargoOwnwer.getOrderHistory = async (req, res) => {
   try {
@@ -31,32 +32,54 @@ cargoOwnwer.getOrderHistory = async (req, res) => {
 };
 
 cargoOwnwer.getActiveOrder = async (req, res) => {
-    try {
-        const orders = await Order.find({ordered_by:req.user._id ,order_status: {$ne: 'dropped_off'}});
-        if(orders && orders.length > 0) {
-            res.status(200).send({
-                statuscode: 200,
-                status: 'success',
-                message: orders
-            })
-        }else if (orders && orders.length === 0) {
-            res.status(200).send({
-                statuscode: 200,
-                status: 'success',
-                message: 'You do not have any active order'
-            })   
-        }
-      } catch (error) {
-          console.log(error);
-        res.status(401).send({
-            statuscode: 200,
-            status: 'error',
-            message: 'Error retrieving your active orders'
-        }) 
-      }
-
+  try {
+    const orders = await Order.find({
+      ordered_by: req.user._id,
+      order_status: { $ne: "dropped_off" },
+    });
+    if (orders && orders.length > 0) {
+      res.status(200).send({
+        statuscode: 200,
+        status: "success",
+        message: orders,
+      });
+    } else if (orders && orders.length === 0) {
+      res.status(200).send({
+        statuscode: 200,
+        status: "success",
+        message: "You do not have any active order",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      statuscode: 200,
+      status: "error",
+      message: "Error retrieving your active orders",
+    });
+  }
 };
 
-cargoOwnwer.getProfile = async (req, res) => {};
+cargoOwnwer.getProfile = async (req, res) => {
+  try {
+    const user = await Cargoowner.find({
+      userDetails: req.user._id,
+    }).populate("userDetails");
+    if (user) {
+      res.status(200).send({
+        statuscode: 200,
+        status: "success",
+        message: user,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      statuscode: 200,
+      status: "error",
+      message: "Error retrieving your user details",
+    });
+  }
+};
 
 module.exports = cargoOwnwer;
