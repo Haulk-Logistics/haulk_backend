60243e214889c07290a6704d7498181b6d83dc3f
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-25 08:43:04 +0100

    finsihed the active orders route

--- controllers/cargoowner.controller.js
+++ controllers/cargoowner.controller.js
@@ -1,40 +1,62 @@
 const cargoOwnwer = {};
-const Order = require('../models/order.model');
+const Order = require("../models/order.model");
 
-cargoOwnwer.getOrderHistory = async (req,res) => {
+cargoOwnwer.getOrderHistory = async (req, res) => {
   try {
-    const orders = await Order.find({ordered_by:req.user._id ,order_status: 'dropped_off'});
-    if(orders && orders.length > 0) {
-        res.status(200).send({
-            statuscode: 200,
-            status: 'success',
-            message: orders
-        })
-    }else if (orders && orders.length === 0) {
-        res.status(200).send({
-            statuscode: 200,
-            status: 'success',
-            message: 'You do not have any order history'
-        })   
+    const orders = await Order.find({
+      ordered_by: req.user._id,
+      order_status: "dropped_off",
+    });
+    if (orders && orders.length > 0) {
+      res.status(200).send({
+        statuscode: 200,
+        status: "success",
+        message: orders,
+      });
+    } else if (orders && orders.length === 0) {
+      res.status(200).send({
+        statuscode: 200,
+        status: "success",
+        message: "You do not have any order history",
+      });
     }
   } catch (error) {
-      console.log(error);
+    console.log(error);
     res.status(401).send({
-        statuscode: 200,
-        status: 'error',
-        message: 'Error retrieving your order history'
-    }) 
+      statuscode: 200,
+      status: "error",
+      message: "Error retrieving your order history",
+    });
   }
+};
+
+cargoOwnwer.getActiveOrder = async (req, res) => {
+    try {
+        const orders = await Order.find({ordered_by:req.user._id ,order_status: {$ne: 'dropped_off'}});
+        if(orders && orders.length > 0) {
+            res.status(200).send({
+                statuscode: 200,
+                status: 'success',
+                message: orders
+            })
+        }else if (orders && orders.length === 0) {
+            res.status(200).send({
+                statuscode: 200,
+                status: 'success',
+                message: 'You do not have any active order'
+            })   
+        }
+      } catch (error) {
+          console.log(error);
+        res.status(401).send({
+            statuscode: 200,
+            status: 'error',
+            message: 'Error retrieving your active orders'
+        }) 
+      }
 
 };
 
-cargoOwnwer.getActiveOrder = async (req,res) => {
-
-};
-
-cargoOwnwer.getProfile = async (req,res) => {
-
-};
-
+cargoOwnwer.getProfile = async (req, res) => {};
 
 module.exports = cargoOwnwer;
