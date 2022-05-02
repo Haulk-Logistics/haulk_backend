705ecc4171570edaa42c533dc15f496dca3a9a2e
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-05-02 16:55:31 +0100

    added user profile details

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1021,6 +1021,23 @@ auth.changePassword = async (req, res, next) => {
     }
 }
 
+auth.getUserProfile = async (req,res) => {
+    try {
+       const user = await User.findOne({_id: req.user.id});
+       res.status(200).json({
+           statuscode: 200,
+           status: "success",
+           message: user
+       })
+    } catch (error) {
+        return res.status(400).json({
+            status: 'error',
+            statusCode: 400,
+            message: 'Error retrieving user details'
+        });
+    }
+}
+
 
 
 
