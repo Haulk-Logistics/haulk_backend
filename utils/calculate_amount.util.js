cb1aedb30996987709941a7a5784a15d57a0b717
bazzscript <bmexit@gmail.com>
2022-05-06 11:18:50 +0100

    fix a  bug

--- utils/calculate_amount.util.js
+++ utils/calculate_amount.util.js
@@ -1,8 +1,10 @@
 const calculate_amount = async (input_distance) => {
     const distance = parseInt(input_distance);
-    const initial_amount = 500;
+    let initial_amount = 500;
+
     const required_amount = Math.round(initial_amount * distance);
-    return required_amount;
+    initial_amount = initial_amount + required_amount;
+    return initial_amount;
 }
 
 module.exports = {
